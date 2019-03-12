0afe04704e5aebf7feefc28d975b8c1383afd466
xuzhanliang <xuzhanliang@yy.com>
2019-03-12 21:23:26 +0800

    ch2

---
+++ src/ch2/constant_test/constant_test.go
@@ -0,0 +1,25 @@
+package constant_test
+
+import "testing"
+
+const (
+	Monday = iota + 1
+	Tuesday
+	Wednesday
+)
+
+const (
+	Readable = 1 << iota
+	Writable
+	Executable
+)
+
+func TestConstantTry(t *testing.T) {
+	t.Log(Monday, Tuesday)
+	t.Log(Readable, Writable, Executable)
+}
+
+func TestConstantTry1(t *testing.T) {
+	a := 1
+	t.Log(a&Readable == Readable, a&Writable == Writable, a&Executable == Executable)
+}
--- src/ch2/fibonacci/fib_test.go
+++ src/ch2/fibonacci/fib_test.go
@@ -5,17 +5,24 @@ import (
 	"testing"
 )
 
-func TestFibList(t *testing.T){
+func TestFibList(t *testing.T) {
 	//var a int = 1
 	//var b int = 1
-	a:=1
-	b:=1
+	a := 1
+	b := 1
 	fmt.Print(a)
-	for i:=0;i<10 ;i++  {
-		fmt.Print(" ",b)
-		tmp:=a
-		a=b
-		b=tmp + a
+	for i := 0; i < 10; i++ {
+		fmt.Print(" ", b)
+		tmp := a
+		a = b
+		b = tmp + a
 	}
 	fmt.Println()
+}
+
+func TestExchange(t *testing.T) {
+	a := 1
+	b := 2
+	a, b = b, a
+	t.Log(a, b)
 }
