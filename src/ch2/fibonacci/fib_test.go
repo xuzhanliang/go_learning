cad7969059eb20ba1d5affd9b09cf49804653687
xuzhanliang <xuzhanliang@yy.com>
2019-03-12 19:04:32 +0800

    Initial commit

---
+++ src/ch2/fibonacci/fib_test.go
@@ -0,0 +1,21 @@
+package fibonacci
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFibList(t *testing.T){
+	//var a int = 1
+	//var b int = 1
+	a:=1
+	b:=1
+	fmt.Print(a)
+	for i:=0;i<10 ;i++  {
+		fmt.Print(" ",b)
+		tmp:=a
+		a=b
+		b=tmp + a
+	}
+	fmt.Println()
+}
